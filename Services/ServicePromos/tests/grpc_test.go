1d66843c10ada14f16b2043e4d5003f34efdd33f
arhefr <efremenkoarhip614090@gmail.com>
2025-06-04 19:07:54 +0300

    add tests

---
+++ Services/ServicePromos/tests/grpc_test.go
@@ -0,0 +1,22 @@
+package test_grpc
+
+import (
+	"promos/internal/app"
+	"testing"
+)
+
+func init() {
+	app.Run()
+}
+
+func TestAdd(t *testing.T) {
+
+}
+
+func TestDelete(t *testing.T) {
+
+}
+
+func TestUse(t *testing.T) {
+
+}
