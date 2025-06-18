dffc1e2b1d2c3d809b284cfcf83af1ce7258fb6e
arhefr <efremenkoarhip614090@gmail.com>
2025-06-18 21:21:29 +0300

    remove useless error

--- service/pkg/errors/errors.go
+++ service/pkg/errors/errors.go
@@ -5,14 +5,7 @@ import (
 )
 
 var (
-	ErrBadArgs               = errors.New("error bad args: 0 <= Currency <= 2 AND Amount >= 0 AND (Uses > 0 OR Uses = -1)")
-	ErrExpAt                 = errors.New("error timestamb of expired data must be more then now")
-	ErrCreatorIsNotOwner     = errors.New("error creator of promo must be have role owner")
-	ErrValueUses             = errors.New("error value of uses must be > 0, for infinity uses set -1")
-	ErrUniquePromo           = errors.New("error name of promo must be unique")
 	ErrExecQuery             = errors.New("error exeсution query")
-	ErrMissingPromoId        = errors.New("error missing promo id")
-	ErrMissingPromoName      = errors.New("error missing promo name")
 	ErrTransactionCommit     = errors.New("error transaction commit")
 	ErrTransactionRollback   = errors.New("error transaction rollback")
 	ErrMissingEnviroment     = errors.New("error missing enviroment")
